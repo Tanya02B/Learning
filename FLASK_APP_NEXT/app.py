54117e0eaee855144626cc293ec357cee07e7b8e
Tanya02B <151550836+Tanya02B@users.noreply.github.com>
2023-12-05 15:14:43 +0300

    Add files via upload

---
+++ FLASK_APP_NEXT/app.py
@@ -0,0 +1,20 @@
+from flask import * # Импорт всего пакета flask
+app = Flask(__name__)  # Создание экземпляра приложения Flask
+
+# Определение маршрута для главной страницы
+@app.route('/')
+def index():
+    return render_template('index.html')  # Возврат шаблона index.html
+
+# Определение маршрута для страницы контактов
+@app.route('/contacts/')
+def contacts():
+    return render_template('contacts.html')  # Возврат шаблона contacts.html
+
+@app.route('/sportsman/')
+def sportsman():
+    return render_template('sportsman.html')  
+
+# Проверка, является ли файл, в котором выполняется данный код, главным
+if __name__ == "__main__":
+     app.run(debug=True)  # Запуск приложения
